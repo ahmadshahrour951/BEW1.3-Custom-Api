9f97b910f7aea863bb01b8e7591c3d10f6878eee
shahrour <ahmadshahrour951@gmail.com>
2021-03-06 09:46:43 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
- 
+const PORT = process.env.PORT || 5000;
+
 const bodyParser = require('body-parser');
 const express = require('express');
+const logger = require('morgan');
 const cors = require('cors');
 
 const db = require('./app/models');
-const routes = require('./app/routes')
+const routes = require('./app/routes');
 
-db.sequelize.authenticate()
+db.sequelize
+  .authenticate()
   .then(() => {
     console.log(
       'Connection to the database has been established successfully.'
@@ -22,14 +25,27 @@ db.sequelize.authenticate()
 const app = express();
 
 app.use(bodyParser.json()); // application/json
+app.use(logger('dev'));
 app.use(cors('*'));
+app.use('/api', routes);
 
-app.use('/', routes);
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
 
-const PORT = process.env.PORT || 5000;
+// const initDb =
+//   process.env.NODE_ENV === 'development'
+//     ? db.sequelize.sync({ force: true })
+//     : db.sequelize.sync();
 
-db.sequelize.sync({ force: true })
+// initDb
+db.sequelize.sync()
   .then(() => {
     app.listen(PORT, console.log(`Server started on port ${PORT}`));
   })
   .catch((err) => console.log('Error: ' + err));
+
+module.exports = app;
